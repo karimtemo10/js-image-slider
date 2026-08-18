e88682f9094cec2a81a3d7b7ed3ba49a7fcf106b
karim <karimdotreda@gmail.com>
2026-08-18 20:54:06 +0300

    create an image slider

--- index.js
+++ index.js
@@ -4,22 +4,60 @@ const viewer = document.querySelector(".images");
 const dots = document.querySelectorAll(".dot");
 
 let counter = 0;
-btnNext.addEventListener("click", () => {
-  if (counter === 8) return;
+
+let autoPlay = setInterval(slideNext, 3000);
+
+function slideNext() {
+  if (counter === 8) {
+    dots[counter].classList.remove("active");
+    dots[0].classList.add("active");
+    counter = 0;
+    viewer.style.transition = "0s";
+    viewer.style.transform = `translateX(${-318 * counter}px)`;
+    viewer.offsetHeight;
+    viewer.style.transition = "1s ease";
+
+    return;
+  }
   counter += 1;
   viewer.style.transform = `translateX(${-318 * counter}px)`;
+  updateDots();
+}
+function slidePrev() {
+  if (counter === 0) {
+    dots[counter + 8].classList.add("active");
+    counter = 8;
+    viewer.style.transition = "0s";
+    viewer.style.transform = `translateX(${-318 * counter}px)`;
+    viewer.offsetHeight;
+    viewer.style.transition = "1s ease";
+
+    updateDots();
+    return;
+  }
+  counter -= 1;
+  viewer.style.transform = `translateX(${-318 * counter}px)`;
+  updateDots();
+}
+
+function updateDots() {
   dots.forEach((dot) => {
     dot.classList.remove("active");
   });
   dots[counter].classList.add("active");
+}
+
+btnNext.addEventListener("click", () => {
+  slideNext();
+  resetTimer();
 });
 
 btnPrev.addEventListener("click", () => {
-  if (counter === 0) return;
-  counter -= 1;
-  viewer.style.transform = `translateX(${-318 * counter}px)`;
-  dots.forEach((dot) => {
-    dot.classList.remove("active");
-  });
-  dots[counter].classList.add("active");
+  slidePrev();
+  resetTimer();
 });
+
+function resetTimer() {
+  clearInterval(autoPlay);
+  autoPlay = setInterval(slideNext, 3000);
+}
